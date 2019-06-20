99e14b2c4026f3627b21131adb441d06e8ca6e6c
Rahulkarki17 <rahulkarki@hotmail.com>
2019-06-20 22:34:38 +0100

    collision

--- space_invaders_1.py
+++ space_invaders_1.py
@@ -3,6 +3,7 @@
 
 import turtle
 import os
+import math
 
 ##Set up the screen
 wn = turtle.Screen()
@@ -42,7 +43,7 @@ enemy.color("red")
 enemy.shape("circle")
 enemy.penup()
 enemy.speed(0)
-enemy.setposition(-200, 250)
+enemy.setposition(-200, -200)
 
 enemyspeed = 2
 
@@ -91,6 +92,13 @@ def fire_bullet():
         bullet.setposition(x, y)
         bullet.showturtle()
 
+def isCollision(t1, t2):
+    distance = math.sqrt(math.pow(t1.xcor()- t2.xcor(),2)+math.pow(t1.ycor() - t2.ycor(),2))
+    if distance < 15:
+        return True
+    else:
+        return False 
+
 ##Create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
@@ -129,6 +137,23 @@ while True:
     if bullet.ycor() > 275:
         bullet.hideturtle()
         bulletstate = "ready"
+
+    #Check for a collision between the bullet and the enemy
+    if isCollision(bullet, enemy):
+        #Reset the bullet
+        bullet.hideturtle()
+        bulletstate = "ready"
+        bullet.setposition(0, -400)
+        #Resert the enemy
+        enemy.setposition(-200, 250)
+
+    if isCollision(player, enemy):
+        player.hideturtle()
+        enemy.hideturtle()
+        print("Game Over")
+        break
+            
+            
 
 
 
